d57b3dd951df6393883e0dd95da5affda48ad94d
Hussnain Ahmad <21100150@lums.edu.pk>
2021-07-19 15:47:47 +0500

    use effect hook re-logins user if cookie is found when remember me is checked while logging in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Alert, Fade } from 'react-bootstrap';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { BrowserRouter, Switch, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useCookies } from 'react-cookie';
 import Dashboard from './components/Dashboard/Dashboard';
-
 import HomeScreen from './components/HomeScreen/HomeScreen';
 import Search from './components/Search/Search';
 import Settings from './components/Settings/Settings';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import PublicRoute from './components/PublicRoute/PublicRoute';
+import { loginSuccessful } from '../src/redux/authSlice';
 function App() {
     const { message, type } = useSelector((state) => state.messageAlert);
+    const { isAuthenticated } = useSelector((state) => state.auth);
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [cookies] = useCookies(['authToken']);
+    const handleCookiesOnStartUp = () => {
+        if (cookies['authToken'] && !isAuthenticated) {
+            dispatch(loginSuccessful());
+        }
+    };
+    useEffect(handleCookiesOnStartUp);
     return (
         <div className="wrapper">
             {message !== '' ? (
