5d8f4277d8b4490e6881954d81ec7024e77b61e7
Hussnain Ahmad <21100150@lums.edu.pk>
2021-08-10 21:38:20 +0500

    removed unnecessary console logs

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
 const { REACT_APP_IS_PRODUCTION, REACT_APP_API_URL, REACT_APP_SENTRY_URL } =
   process.env;
-console.log(process.env);
 export const IS_PRODUCTION = REACT_APP_IS_PRODUCTION === 'true';
 export const API_BASE_PATH = REACT_APP_API_URL.toString();
 export const SENTRY_URL = REACT_APP_SENTRY_URL.toString();
